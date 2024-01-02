4d813dda6d79e306588f635089da9a8dce6faf04
Fatih <yildizafatih@gmail.com>
2024-01-02 19:42:43 +0100

    image slider added

--- script.js
+++ script.js
@@ -14,3 +14,54 @@ for (let i = 0; i < dropdowns.length; i++) {
         }
     })
 }
+
+let images = document.querySelectorAll("img");
+let imgCircles = document.querySelectorAll(".image-circle");
+let currentImgIndex = 0;
+images[0].style.display = "inline-block";
+imgCircles[currentImgIndex].style.backgroundColor = "red";
+let imgBackButton = document.querySelector(".back-button");
+let imgNextButton = document.querySelector(".next-button");
+
+imgBackButton.addEventListener("click", backButtonHandler);
+
+imgNextButton.addEventListener("click", nextButtonHandler)
+
+for (let i = 0; i < imgCircles.length; i++) {
+    imgCircles[i].addEventListener("click", function () {
+        hideAllImages();
+        displayImage(i);
+        currentImgIndex = i;
+    });
+}
+
+setInterval(() => {
+    nextButtonHandler();
+}, 5000);
+
+function backButtonHandler() {
+    hideAllImages();
+    currentImgIndex = (currentImgIndex - 1 + images.length) % images.length;
+    displayImage(currentImgIndex);
+}
+
+function nextButtonHandler() {
+    hideAllImages();
+    currentImgIndex = (currentImgIndex + 1 + images.length) % images.length;
+    displayImage(currentImgIndex);
+}
+
+function displayImage(index) {
+    images[index].style.display = "inline-block";
+    imgCircles[index].style.backgroundColor = "red";
+}
+
+function hideAllImages() {
+    for (let i = 0; i < images.length; i++) {
+        images[i].style.display = "none";
+    }
+
+    for (let i = 0; i < imgCircles.length; i++) {
+        imgCircles[i].style.backgroundColor = "";
+    }
+}
